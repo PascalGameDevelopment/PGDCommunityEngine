62df6667e859b73428dc073ee6a7017db70d0aa1
George Bakhtadze <avagames@gmail.com>
2015-01-06 05:33:19 +0400

    Simple examle (Windows)

--- Examples/WinTest/0c.bat
+++ Examples/WinTest/0c.bat
@@ -4,7 +4,7 @@ set fpcfile=WinTest.dpr
 set fpccmd=%FPC_HOME%\bin\i386-win32\fpc.exe -dDEBUG -Sa
 rem set fpccmd=%FPC_HOME%\bin\i386-win32\fpc.exe -dRELESE -O3 -Sa
 
-set fpcoptions=-Fu%SRC_DIR% -Fu%SRC_DIR%/Entity -Fu%SRC_DIR%/Template -Fu%SRC_DIR%/Application -Fi%SRC_DIR% -Fi%SRC_DIR%/Template
+set fpcoptions=-Fu%SRC_DIR% -Fu%SRC_DIR%/Entity -Fu%SRC_DIR%/Template -Fu%SRC_DIR%/Application -Fu%SRC_DIR%/Renderer -Fi%SRC_DIR% -Fi%SRC_DIR%/Template
 set fpcoptions=%fpcoptions% -FE%SRC_DIR%/../bin -FU%SRC_DIR%/../output -B
 rem set fpcoptions=%fpcoptions% -Sa
 
--- Examples/WinTest/wintest.dpr
+++ Examples/WinTest/wintest.dpr
@@ -10,7 +10,7 @@
   either express or implied.  See the license for the specific language governing
   rights and limitations under the license.
 
-  The Original Code is wintest.pas
+  The Original Code is wintest.dpr
 
   The Initial Developer of the Original Code is documented in the accompanying
   help file PGDCE.chm.  Portions created by these individuals are Copyright (C)
@@ -31,17 +31,68 @@ program wintest;
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils, CEWindowsApplication;
+  SysUtils, dglOpenGL, Windows,
+  CEWindowsApplication, CEBaseRenderer, CEOpenGL4Renderer, CEMesh,
+  CEBaseTypes, CEVectors;
+
+type
+  // Example mesh class
+  TCERotatingTriangleMesh = class(TCEMesh)
+  private
+    FAngle: Single;
+    procedure SetAngle(const Value: Single);
+  public
+    property Angle: Single read FAngle write SetAngle;
+    procedure FillVertexBuffer(Dest: Pointer); override;
+  end;
+
+procedure TCERotatingTriangleMesh.SetAngle(const Value: Single);
+begin
+  FAngle := Value;
+  VertexBuffer.Status := tsChanged; // Invalidate buffer
+end;
+
+procedure TCERotatingTriangleMesh.FillVertexBuffer(Dest: Pointer);
+var
+  a: Single;
+  v: ^TCEVector3fArray;
+begin
+  inherited;
+  a := Angle * pi/180;
+  v := Dest;
+  Vec3f(cos(a), -sin(a), 0, v^[0]);
+  Vec3f(cos(a+2*pi/3), -sin(a+2*pi/3), 0, v^[1]);
+  Vec3f(cos(a+4*pi/3), -sin(a+4*pi/3), 0, v^[2]);
+  FVerticesCount := 3;
+end;
 
 var
   App: TCEWindowsApplication;
-
+  Renderer: TCEOpenGL4Renderer;
+  Mesh: TCERotatingTriangleMesh;
 begin
   {$IF Declared(ReportMemoryLeaksOnShutdown)}
   ReportMemoryLeaksOnShutdown := True;
   {$IFEND}
   App := TCEWindowsApplication.Create();
-  while not App.Terminated do
-    App.Process();
+  Renderer := TCEOpenGL4Renderer.Create(App);
+  Mesh := TCERotatingTriangleMesh.Create();
+
+  glLoadIdentity;
+  glTranslatef(0, 0, -3);
+
+  while not App.Terminated do begin
+      Renderer.Clear([cfColor, cfDepth], GetColor(0, 0, 0, 0), 1.0, 0);
+
+      App.Process();
+      Mesh.Angle := (GetTickCount mod 3600) * 0.1;
+      Renderer.RenderMesh(Mesh);
+
+      Renderer.NextFrame();
+  end;
+
+  Mesh.Free();
+  Renderer.Free();
   App.Free();
+  Readln;
 end.
