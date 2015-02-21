1313a05539a856031b0a41d696c6960424629c23
George Bakhtadze <avagames@gmail.com>
2015-02-21 23:28:24 +0400

    Material class

--- Source/Entity/CEMaterial.pas
+++ Source/Entity/CEMaterial.pas
@@ -46,7 +46,7 @@ type
   end;
   TTextureSetups = array[0..MAX_TEXTURES-1] of TTextureSetup;
 
-  // Representing render pass setup
+  // Encapsulates render state, texture and shader settings
   TCERenderPass = class(TCEBaseEntity)
   private
     // Shader program ID used by a renderer
@@ -64,6 +64,35 @@ type
     property Texture0: TCEImageResource read GetTexture0 write SetTexture0;
     property VertexShader: TCETextResource read FVertexShader write FVertexShader;
     property FragmentShader: TCETextResource read FFragmentShader write FFragmentShader;
+  end;
+
+  // Set of render passes, represents render technique
+  TCERenderTechnique = class(TCEBaseEntity)
+  private
+    FTotalPasses: Integer;
+    PassesCache: array of TCERenderPass;
+    FValid: Boolean;
+    procedure SetTotalPasses(const Value: Integer);
+    function GetPass(Index: Integer): TCERenderPass;
+    procedure SetPass(Index: Integer; const Value: TCERenderPass);
+  public
+    LOD: Single;
+    property TotalPasses: Integer read FTotalPasses write SetTotalPasses;
+    property Passes[Index: Integer]: TCERenderPass read GetPass write SetPass; default;
+    property Valid: Boolean read FValid write FValid;
+  end;
+
+  // Set of techniques
+  TCEMaterial = class(TCEBaseEntity)
+  private
+    FTotalTechniques: Integer;
+    procedure SetTotalTechniques(const Value: Integer);
+    function GetTechnique(Index: Integer): TCERenderTechnique;
+    procedure SetTechnique(Index: Integer; const Value: TCERenderTechnique);
+  public
+    function GetTechniqueByLOD(Lod: Single): TCERenderTechnique;
+    property TotalTechniques: Integer read FTotalTechniques write SetTotalTechniques;
+    property Technique[Index: Integer]: TCERenderTechnique read GetTechnique write SetTechnique; default;
   end;
 
   function _GetTextureId(Pass: TCERenderPass; Index: Integer): Integer;
@@ -113,5 +142,66 @@ begin
     TextureSetups[i].TextureId := ID_NOT_INITIALIZED;
 end;
 
-end.
+{ TCERenderTechnique }
 
+function PassPropertyName(Index: Integer): string;
+begin
+  Result := 'Pass ' + IntToStr(Index);
+end;
+
+procedure TCERenderTechnique.SetTotalPasses(const Value: Integer);
+begin
+  FTotalPasses := Value;
+  SetLength(PassesCache, FTotalPasses);
+end;
+
+function TCERenderTechnique.GetPass(Index: Integer): TCERenderPass;
+begin
+  Assert(Index < TotalPasses);
+  Assert(Length(PassesCache) = TotalPasses, ClassName() + '("' + GetFullName() + '")');
+  if not Assigned(PassesCache[Index]) then
+    Result := PassesCache[Index]
+  else begin
+    PassesCache[Index] := ResolveObjectLink(PassPropertyName(Index)) as TCERenderPass;
+    Result := PassesCache[Index]
+  end;
+end;
+
+procedure TCERenderTechnique.SetPass(Index: Integer; const Value: TCERenderPass);
+begin
+  SetObjectLink(PassPropertyName(Index), IFF(Assigned(Value), Value.GetFullName, ''));
+end;
+
+{ TCEMaterial }
+
+function TechPropertyName(Index: Integer): string;
+begin
+  Result := 'Tech ' + IntToStr(Index);
+end;
+
+procedure TCEMaterial.SetTotalTechniques(const Value: Integer);
+begin
+  FTotalTechniques := Value;
+end;
+
+function TCEMaterial.GetTechnique(Index: Integer): TCERenderTechnique;
+begin
+  Result := ResolveObjectLink(PassPropertyName(Index)) as TCERenderTechnique;
+end;
+
+procedure TCEMaterial.SetTechnique(Index: Integer; const Value: TCERenderTechnique);
+begin
+  SetObjectLink(TechPropertyName(Index), IFF(Assigned(Value), Value.GetFullName, ''));
+end;
+
+function TCEMaterial.GetTechniqueByLOD(Lod: Single): TCERenderTechnique;
+var i: Integer;
+begin
+  Result := nil;
+  i := 0;
+  while (i < TotalTechniques) and
+        ( {not (isVisible in Technique[i].State) or }not Technique[i].Valid or (Technique[i].LOD > Lod) ) do Inc(i);
+  if i < TotalTechniques then Result := Technique[i];
+end;
+
+end.
